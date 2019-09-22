7c8e74ded3c433dabaa0ae66a6ea4cacbfd5057e
Seeker-Devs <49820135+Seeker-Devs@users.noreply.github.com>
2019-09-22 15:34:05 +0500

    Add files via upload

---
+++ ConditionsNLoops/IfElseStatements.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+//now this gonna be fun duddee, let me give a copyright claim
+/**
+This is Seeker.. oh yes, these comments work like in php. awesome. this is for educational purposes
+**/
+int main()
+{
+	int age;
+	cin >> age;
+	if (age >= 40) {
+	    cout << "Woahh.. you are way too older for the job. Sorry!";	
+	}
+	if (age <= 24) {
+	    cout << "Sorry young one, you are way too young, and yes,\n your biological age, not your age on your Id card..";
+	}
+	else {
+	    cout << "MANY BIG GGS AND CONGRATS!\n You have been accepted and hired in the SeekX company!";
+	}
+}
+	            
