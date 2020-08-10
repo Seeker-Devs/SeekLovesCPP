9af5e24f092623e0be3943cfef1e3b9623ee00c5
Seeker-Devs <49820135+Seeker-Devs@users.noreply.github.com>
2020-08-10 12:45:09 +0500

    Fix typos

--- Basic/BasicVars.cpp
+++ Basic/BasicVars.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-	int firstcVar = 11;
-	cout << "My age is " << firstcVar;
-	//Inputs My age is 11. Cool I guess?
+	int integvar= 18;
+	cout << "My age is " << integvar;
+	//Outputs My age is 18. Cool I guess?
 }
